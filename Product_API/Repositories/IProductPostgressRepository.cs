717613fb6f0b957c82a42a5c2bf690e580394763
AbdullohDev07 <gamesupro01@gmail.com>
2024-02-14 06:57:14 +0500

    Info Update

--- Product_API/Repositories/IProductPostgressRepository.cs
+++ Product_API/Repositories/IProductPostgressRepository.cs
@@ -1,17 +1,59 @@
-﻿using Product_API.Models;
+﻿using Npgsql;
+using Product_API.Models;
 
 namespace Product_API.Repositories
 {
     public class ProductPostgressRepository : IProductRepository
     {
+        public string ConnectionString = "Server=localhost;Port=16172;Database=your_database;username=postgres;Password=axihub;";
         public Product Add(Product product)
         {
-            throw new NotImplementedException();
+            using (NpgsqlConnection connection = new NpgsqlConnection(ConnectionString))
+            {
+
+                connection.Open();
+                try
+                {
+                    using NpgsqlCommand cmd = new NpgsqlCommand(@$"insert into products(Name,Description,PhotoPath) values ('{product.Name}','{product.Description}','{product.PhotoPath}') ", connection);
+                    cmd.ExecuteNonQuery();
+
+
+                }
+                catch
+                {
+                }
+            }
+            return product;
         }
 
         public List<Product> GetAll()
         {
-            throw new NotImplementedException();
+            List<Product> products = new List<Product>();
+            using (NpgsqlConnection connection = new NpgsqlConnection(ConnectionString))
+            {
+
+                connection.Open();
+                try
+                {
+                    Product product = new Product();
+                    using NpgsqlCommand cmd = new NpgsqlCommand(@$"select * from product ", connection);
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        product.Name = reader.GetString(0);
+                        product.Description = reader.GetString(1);
+                        product.PhotoPath = reader.GetString(2);
+                        products.Add(product);
+                    }
+
+                }
+                catch
+                {
+                }
+                return products;
+            }
         }
+
+
     }
 }
